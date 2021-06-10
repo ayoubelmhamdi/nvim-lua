e9b6bf85c6e6a9cad74a0853446653183f780092
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-06-10 19:04:00 +0100

    renamr Dirs to nvDirs

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
+vim.cmd('let startvim = reltime()') -- at the beginin
 -- INIT NVIM
 
 
 require('nvPlugins')
-require('luaSettings')
+require('nvSettings')
 require('nvLSP')
 require('nvIDE')
 
 
 
 -- config in vimScript
-vim.cmd('let startvim = reltime()') -- at the beginin
 vim.cmd('source ~/.config/nvim/vimScript/init.vim')
-
 vim.cmd('echo reltimestr(reltime(startvim))') -- at the end
+
 
 
