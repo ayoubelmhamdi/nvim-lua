c051c15b23c1ec5127690dac9d0b9aaced283152
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-06-18 13:02:57 +0100

    maping chage to c-up9

--- lua/nvPlugins/shade.lua
+++ lua/nvPlugins/shade.lua
@@ -2,8 +2,8 @@ require'shade'.setup({
   overlay_opacity = 50,
   opacity_step = 1,
   keys = {
-    brightness_up    = '<C-Up>',
-    brightness_down  = '<C-Down>',
-    toggle           = '<Leader>s',
+    brightness_up    = '<C-Up>9',
+    brightness_down  = '<C-Down>9',
+    toggle           = '<Leader>9s',
   }
 })
