9ab4b0b78340409bb7287d68ac68a7932d9e46f5
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-06-26 17:50:29 +0100

    modifile config from sumnko repo

--- lua/nvLSP/lua-ls.lua
+++ lua/nvLSP/lua-ls.lua
@@ -19,9 +19,6 @@ local sumneko_root_path = '/'.. USER ..'/.config/nvim/lspconfig/sumneko_lua/lua-
 local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
 
 
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
 
 
 require'lspconfig'.sumneko_lua.setup {
@@ -33,7 +30,14 @@ require'lspconfig'.sumneko_lua.setup {
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
         version = 'LuaJIT',
         -- Setup your lua path
-        path = runtime_path,
+        path = {
+            '?.lua',
+            '?/init.lua',
+            vim.fn.expand'~/.luarocks/share/lua/5.3/?.lua',
+            vim.fn.expand'~/.luarocks/share/lua/5.3/?/init.lua',
+            '/usr/share/5.3/?.lua',
+            '/usr/share/lua/5.3/?/init.lua'
+          }
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
@@ -45,7 +49,12 @@ require'lspconfig'.sumneko_lua.setup {
       workspace = {
         -- Make the server aware of Neovim runtime files
         preloadFileSize = 500,
-        library = vim.api.nvim_get_runtime_file("", true),
+        -- library = vim.api.nvim_get_runtime_file("", true),
+        -- library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true},
+        library = {
+          [vim.fn.expand'~/.luarocks/share/lua/5.3'] = true,
+          ['/usr/share/lua/5.3'] = true
+       }
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
