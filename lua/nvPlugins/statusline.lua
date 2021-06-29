25230fae1699ee2c6280a972d9499e5fe4e02fd8
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-06-29 19:41:55 +0100

    update status

--- lua/nvPlugins/statusline.lua
+++ lua/nvPlugins/statusline.lua
@@ -1,6 +1,28 @@
 -- look at : https://github.com/hoob3rt/lualine.nvim
 require'lualine'.setup {
-  options = {lower = true},
-  sections = {lualine_a = {{'mode', lower = false}}, lualine_b = {'branch'}}
+  options = {
+    icons_enabled = true,
+    theme = 'gruvbox',
+    component_separators = {'', ''},
+    section_separators = {'', ''},
+    disabled_filetypes = {}
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch'},
+    lualine_c = {'filename'},
+    lualine_x = {'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
 }
-
