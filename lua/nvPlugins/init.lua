22167b167c07f5dba3687dfbf952be277136fe11
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-07-01 19:43:26 +0100

    if config ovveride by update :some plugin stoped

--- lua/nvPlugins/init.lua
+++ lua/nvPlugins/init.lua
@@ -1,8 +1,8 @@
 require('nvPlugins.packerPlugins')
 require('nvPlugins.telescope')
-
-
-require('nvPlugins.shade')
+--
+--
+--require('nvPlugins.shade')
 require('nvPlugins.spellsitter')
 require('nvPlugins.tabline')
 require('nvPlugins.statusline')
