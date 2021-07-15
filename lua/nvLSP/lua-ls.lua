cafb37c2e4f5aca0893d08a35870a2fb0922edec
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-07-15 08:15:09 +0100

    fix max load bayte

--- lua/nvLSP/lua-ls.lua
+++ lua/nvLSP/lua-ls.lua
@@ -1,30 +1,12 @@
 -- lua server module
-local system_name
+
 USER = vim.fn.expand('$USER')
-
-if vim.fn.has("mac") == 1 then
-  system_name = "macOS"
-elseif vim.fn.has("unix") == 1 then
-  system_name = "Linux"
-elseif vim.fn.has('win32') == 1 then
-  system_name = "Windows"
-else
-  print("Unsupported system for sumneko")
-end
-
-
-
--- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
--- local sumneko_root_path = '/'.. USER ..'/.config/nvim/lspconfig/sumneko_lua/lua-language-server'
--- local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
-
 local sumneko_root_path = '/'.. USER ..'/.config/nvim/lua-language-server'
-local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
+local sumneko_binary = sumneko_root_path.."/bin/Linux/lua-language-server"
 
 
 require'lspconfig'.sumneko_lua.setup {
   cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
-  -- capabilities = capabilities,
   settings = {
     Lua = {
       runtime = {
@@ -44,12 +26,14 @@ require'lspconfig'.sumneko_lua.setup {
         -- Get the language server to recognize the `vim` global
         -- globals = {'vim'},
         enable = true,
-        globals = {"vim", "map", "filter", "range", "reduce", "head", "tail", "nth", "use"},
+        globals = {"vim","library", "map", "filter", "range", "reduce", "head", "tail", "nth", "use"},
         disable = {"lowercase-global"}
       },
+      completion = { callSnippet = "Both" },
       workspace = {
         -- Make the server aware of Neovim runtime files
-        preloadFileSize = 5000,
+        maxPreload = 2000,
+        preloadFileSize = 50000,
         -- library = vim.api.nvim_get_runtime_file("", true),
         -- library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true},
         library = {
