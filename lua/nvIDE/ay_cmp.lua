36cf49ad566eeaac9d844f59bca8673786e55263
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-11-05 00:42:03 +0100

    cmp+lusnip v3: s mode too

--- lua/nvIDE/ay_cmp.lua
+++ lua/nvIDE/ay_cmp.lua
@@ -1,7 +1,6 @@
 -- to make snippet change whe add any char
 -- know i'm disable because TextChangedI make text write randomly on screen
 -- require'luasnip'.config.setup({updateevents="TextChangedI" })
-
 -- to load vs code snippet
 -- must loaded after luasnip call snippet
 vim.cmd('set rtp+=/projects/lua/friendly-snippets')
@@ -19,6 +18,10 @@ vim.cmd([[
 -- vim.o.completeopt = "menu,menuone,noselect"
 vim.o.completeopt = 'menuone,noselect'
 
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 -- luasnip setup
 local luasnip = require 'luasnip'
 
@@ -38,31 +41,30 @@ cmp.setup {
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
         ['<CR>'] = cmp.mapping.confirm {behavior = cmp.ConfirmBehavior.Replace, select = true},
-        ['<Tab>'] = function(fallback)
+        ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
             else
                 fallback()
             end
-        end,
-        ['<S-Tab>'] = function(fallback)
+        end, {"i", "s"}),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+                luasnip.jump(-1)
             else
                 fallback()
             end
-        end
+        end, {"i", "s"})
     },
     sources = {
-        {name = 'path'},
-        {name = 'luasnip', opts = {use_show_condition = false}},
-        {name = 'nvim_lsp'},
-        {name = 'cmp_tabnine'},
-        {name = 'treesitter'},
+        {name = 'path'}, {name = 'luasnip', opts = {use_show_condition = false}}, {name = 'nvim_lsp'}, {name = 'cmp_tabnine'}, {name = 'treesitter'},
         {name = 'buffer'}
         -- { name = 'nvim_lua', keyword_lengh=1 },
         -- { name = 'look' , keyword_lengh=0 },
@@ -98,5 +100,5 @@ cmp.setup {
             return vim_item
         end
     }
-
 }
+
