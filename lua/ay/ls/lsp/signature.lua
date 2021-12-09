f8d50ebd8e6473d82319bc1e7edaa243d08c3b7c
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-12-09 12:45:17 +0100

    fix signature to auto close after insertleve or change the word

--- lua/ay/ls/lsp/signature.lua
+++ lua/ay/ls/lsp/signature.lua
@@ -1,8 +1,8 @@
 
 signature_config = {
   log_path = vim.fn.expand("$HOME") .. "/tmp/sig.log",
-  debug = true,
-  hint_enable = false,
-  handler_opts = {border = "single"},
-  max_width = 80
+  debug = false,
+  hint_enable = true,
+  -- handler_opts = {border = "single"},
+  max_width = 80,
 }
