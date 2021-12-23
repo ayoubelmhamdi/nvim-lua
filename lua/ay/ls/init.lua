542136d5954b2c07e5b5afede48fea56b1164e89
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-12-24 00:48:37 +0100

    v3

---
+++ lua/ay/ls/init.lua
@@ -0,0 +1,10 @@
+
+  require('ay/ls/signature')
+  require('ay/ls/bash/bashls')
+  require('ay/ls/c/clangd')
+  require('ay/ls/latex/texlab')
+  require('ay/ls/Lua/lua-ls')
+  require('ay/ls/markdown/zeta-note')
+  require('ay/ls/javascript/deno')
+  require('ay/ls/javascript/tsserver')
+
